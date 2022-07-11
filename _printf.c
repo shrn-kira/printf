c97da4df2127553db4439e8001774eded0a5f134
NyokabiKamau <nyokabikamau2014@gmail.com>
2022-07-11 17:01:50 +0000

    _printf.c

--- _printf.c
+++ _printf.c
@@ -1,22 +1,51 @@
 #include "main.h"
-#include <stdio.h>
-#include <stdarg.h>
 
 /**
- * _printf - function that produces output according to a format
- * @format: character string
+ * _printf - formatted output conversion and print data.
+ * @format: input string.
  *
- * Return: num of characters printed ecluding \0 at end of string
- * */
+ * Return: number of chars printed.
+ */
 int _printf(const char *format, ...)
 {
-	va_list args;
-	int output;
+	unsigned int i = 0, len = 0, ibuf = 0;
+	va_list arguments;
+	int (*function)(va_list, char *, unsigned int);
+	char *buffer;
 
-	va_start (args, format);
-	output = vprintf (stdout, format, args);
-	va_end (args);
-
-	return (output)
+	va_start(arguments, format), buffer = malloc(sizeof(char) * 1024);
+	if (!format || !buffer || (format[i] == '%' && !format[i + 1]))
+		return (-1);
+	if (!format[i])
+		return (0);
+	for (i = 0; format && format[i]; i++)
+	{
+		if (format[i] == '%')
+		{
+			if (format[i + 1] == '\0')
+			{	print_buf(buffer, ibuf), free(buffer), va_end(arguments);
+				return (-1);
+			}
+			else
+			{	function = get_print_func(format, i + 1);
+				if (function == NULL)
+				{
+					if (format[i + 1] == ' ' && !format[i + 2])
+						return (-1);
+					handl_buf(buffer, format[i], ibuf), len++, i--;
+				}
+				else
+				{
+					len += function(arguments, buffer, ibuf);
+					i += ev_print_func(format, i + 1);
+				}
+			} i++;
+		}
+		else
+			handl_buf(buffer, format[i], ibuf), len++;
+		for (ibuf = len; ibuf > 1024; ibuf -= 1024)
+			;
+	}
+	print_buf(buffer, ibuf), free(buffer), va_end(arguments);
+	return (len);
 }
-
