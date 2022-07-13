98047cac305dad3ccc37954ec1c347342479f921
shrn-kira <shrnkira@gmail.com>
2022-07-13 23:40:05 +0000

    Updating the ev_print file

--- ev_print.c
+++ ev_print.c
@@ -1,11 +1,11 @@
 #include "main.h"
 
 /**
- * ev_print - returns the amount of identifiers.
- * @s: argument indentifier
- * @index: index of argument identifier.
- * Return: amount of identifiers.
- */
+* ev_print - returns the amount of identifiers.
+* @s: argument indentifier
+* @index: index of argument identifier.
+* Return: amount of identifiers.
+*/
 int ev_print(const char *s, int index)
 {
 	print_t pr[] = {
@@ -17,7 +17,25 @@ int ev_print(const char *s, int index)
 		{"X", print_upx}, {"S", print_S},
 		{"p", print_p}, {"x", print_hex},
 		{" o", print_oct}, {"r", print_rev},
-		{"R", print_rot13},{NULL, NULL},
+		{"R", print_rot13}, {"o", print_oct},
+		{"lx", print_lo_hex}, {"li", print_lo_i},
+		{"ld", print_lo_i}, {"#X", print_upx},
+		{"lo", print_lo_oct}, {"lu", print_lo_uint},
+		{"lX", print_lo_upx}, {"#x", print_n_hex},
+		{"#o", print_n_oct}, {"hx", print_s_hex},
+		{"hi", print_s_int}, {"hd", print_s_int},
+		{"ho", print_s_oct}, {"#i", print_i},
+		{"#d", print_i}, {"#u", print_unt},
+		{"+i", print_unt}, {"+d", print_unt},
+		{"+u", print_unt}, {"+o", print_oct},
+		{"+x", print_hex}, {"+X", print_upx},
+		{" u", print_unt},  {"p", print_a},
+		{" x", print_hex}, {" X", print_upx},
+		{"%", print_prg}, {" +i", print_unt},
+		{" +d", print_unt}, {"+ i", print_unt},
+		{"hu", print_s_uint}, {"hX", print_s_upx},
+		{" i", print_spc_int}, {" d", print_spc_int},
+		{"+ d", print_unt}, {NULL, NULL},
 	};
 	int i = 0, j = 0, first_index;
 
