2dc5722a542f449a10ad1350b005960f31efd9ff
shrn-kira <shrnkira@gmail.com>
2022-07-15 00:10:31 +0000

    Updates

---
+++ print_oct.c
@@ -0,0 +1,45 @@
+#include "main.h"
+
+/**
+*  print_oct - prints decimal number in octal
+*  @args: input number
+*  @buf: buff pointer
+*  @ibuf: index for buff pointer
+*
+*  Return: number of chars printed.
+*/
+int print_oct(va_list args, char *buf, unsigned int ibuf)
+{
+	int int_input, i, isnegative, count, first_digit;
+	char *octal, *binary;
+
+	int_input = va_arg(args, int);
+	isnegative = 0;
+	if (int_input == 0)
+	{
+		ibuf = handle_buffer(buf, '0', ibuf);
+		return (1);
+	}
+	if (int_input < 0)
+	{
+		int_input = (int_input * -1) - 1;
+		isnegative = 1;
+	}
+	binary = malloc(sizeof(char) * (32 + 1));
+	binary = binary_array(binary, int_input, isnegative, 32);
+	octal = malloc(sizeof(char) * (11 + 1));
+	octal = fill_oct_array(binary, octal);
+	for (first_digit = i = count = 0; octal[i]; i++)
+	{
+		if (octal[i] != '0' && first_digit == 0)
+			first_digit = 1;
+		if (first_digit)
+		{
+			ibuf = handle_buffer(buf, octal[i], ibuf);
+			count++;
+		}
+	}
+	free(binary);
+	free(octal);
+	return (count);
+}
--- printint.c
+++ printint.c
@@ -1,42 +1,39 @@
 #include "main.h"
 
 /**
-* print_i - prints an integer
-* @args: input string
-* @ibuf: index for buff pointer
-* @buf: buff pointer
-* Return: the decimal function
+*  printint - prints integer with a plus symbol
+*  @args: input string
+*  @ibuf: index for buff pointer
+*  @buf: buff pointer
+*
+*  Return: counter
 */
-int print_i(va_list args, char *buf, unsigned int ibuf)
+int printint(va_list args, char *buf, unsigned int ibuf)
 {
+	unsigned int int_in, temp, i, div;
 	int input;
-	unsigned int int_in, temp, i, div, isneg;
 
 	input = va_arg(args, int);
-	isneg = 0;
 	if (input < 0)
 	{
 		int_in = input * -1;
 		ibuf = handle_buffer(buf, '-', ibuf);
-		isneg = 1;
 	}
 	else
 	{
 		int_in = input;
+		ibuf = handle_buffer(buf, '+', ibuf);
 	}
-
 	temp = int_in;
 	div = 1;
-
 	while (temp > 9)
 	{
 		div *= 10;
 		temp /= 10;
 	}
-
 	for (i = 0; div > 0; div /= 10, i++)
 	{
 		ibuf = handle_buffer(buf, ((int_in / div) % 10) + '0', ibuf);
 	}
-	return (i + isneg);
+	return (i + 1);
 }
